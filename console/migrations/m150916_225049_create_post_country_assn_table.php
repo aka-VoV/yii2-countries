2f8a3da5ee11a788c4446531a33b3de2fffebaf2
akaVoV <vlvladimirvl@gmail.com>
2015-09-17 03:34:46 +0300

    Add data and migrations

---
+++ console/migrations/m150916_225049_create_post_country_assn_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use yii\db\Migration;
+
+class m150916_225049_create_post_country_assn_table extends Migration
+{
+    public function safeUp()
+    {
+        $this->createTable('{{%post_country_assn}}', [
+            'post_id'   => $this->integer(11)->notNull(),
+            'country_id'    => $this->integer(11)->notNull(),
+        ]);
+        $this->addPrimaryKey('', '{{%post_country_assn}}', ['post_id', 'country_id']);
+    }
+
+    public function safeDown()
+    {
+        echo "Removing tables.\n";
+        $this->dropTable('{{%post_country_assn}}');
+    }
+
+}
